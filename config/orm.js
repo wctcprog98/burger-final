f519a607ed33df23552806f495088b764dc4acc4
root <chinenyediei@gmail.com>
2019-09-09 04:38:09 -0500

    server set up, database setup, routes setup

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function (tableInput, cb) {
+        connection.query("SELECT * FROM" + tableInput + ";", function (err, result) {
+            if (err) throw err;
+            cb(result); 
+        })
+    }
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars")
+var methodOverride = require("method-override");
+
+var app = express();
+app.use(bodyParser.urlencoded({
+extended: false 
+}))
+
+app.use(express.static(__dirname + "public"));
+
+app.use(methodOverride("_method"));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+})); 
+
+
+//view engine 
+app.set("view engine", "handlebars");
+//set port to 8080 for Heroku
+var PORT = process.env.PORT || 8080;
+
+//setup routes
+var routes = require("./controllers/burgers_controller");
+app.use("/", routes);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
